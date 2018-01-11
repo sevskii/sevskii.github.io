9db59191cfb12b02022b09d568036bf949c9226e
sevskii <sevskii111@gmail.com>
2018-01-11 04:25:44 +0300

    made secret page pretty

--- randomsite/main.js
+++ randomsite/main.js
@@ -1,15 +1,20 @@
-
-Date.prototype.addHours = function(h) {    
-   this.setTime(this.getTime() + (h*60*60*1000)); 
-   return this;   
+Date.prototype.addHours = function (h) {
+    this.setTime(this.getTime() + (h * 60 * 60 * 1000));
+    return this;
 }
+
 var date = new Date();
 date = date.addHours(-7);
-Math.seedrandom(date.getDay() * 1241 + "frewqrqS");
+
 
 console.log(date);
 $(document).ready(function () {
-    $("#mr").text(parseInt(Math.random() * 5) + 1);
-    $("#nr").text(parseInt(Math.random() * 5) + 1);
-    $("#sr").text(parseInt(Math.random() * 5) + 1);
+    $.get("https://randomizerforsevs.herokuapp.com/getseed", function (data) {
+        Math.seedrandom(data + "frewqrqS");
+        $("#mr").text(parseInt(Math.random() * 5) + 1);
+        $("#nr").text(parseInt(Math.random() * 5) + 1);
+        $("#sr").text(parseInt(Math.random() * 5) + 1);
+    });
+
+
 });
