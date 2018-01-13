0004cae800ad3093ffb68c09c67275435f34b482
sevskii <sevskii111@gmail.com>
2018-01-13 14:47:11 +0300

    updated random

--- randomsite/main.js
+++ randomsite/main.js
@@ -1,9 +1,8 @@
 $(document).ready(function () {
     $.get("https://randomizerforsevs.herokuapp.com/getseed", function (data) {
-        Math.seedrandom(data + "frewqrqS");
-        $("#mr").text(parseInt(Math.random() * 5) + 1);
-        $("#nr").text(parseInt(Math.random() * 5) + 1);
-        $("#sr").text(parseInt(Math.random() * 5) + 1);
+        $("#mr").text(data[0]);
+        $("#nr").text(data[2]);
+        $("#sr").text(data[4]);
     });
     setInterval(updateTimer, 1000);
     var currDate = new Date();
